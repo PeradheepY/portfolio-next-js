2a95358578fe449b844fd465dc1c37e1f8cdfadd
[PeradheepY] <[peradheep200617@outlook.com]>
2025-07-19 20:28:05 +0530

    updated resume

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
         transition={{ duration: 0.6, delay: 1.2 }}
 
 
-          href="/Peradheep_Resume(without_photo).pdf"
+          href="/PeradheepY_Resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
